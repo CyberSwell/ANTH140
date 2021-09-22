af9e3a01669d4dcd7620cba6d359a610fa781407
CyberSwell <jameswzhang@gmail.com>
2021-09-21 23:11:52 -0400

    finished script v1

--- AlcCounter.sh
+++ AlcCounter.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-var=`date +"%FORMAT_STRING"`
-now=`date +"%Y-%m-%d-%H-%M"`
-FILE_NAME=$now
+# Get current date
+FILE_DATE=`date +"%Y-%m-%d-%H-%M"`
 
-curl -s "https://www.finewineandgoodspirits.com/webapp/wcs/stores/servlet/StoreStateSearch?storeId=10051&langId=-1&catalogId=10051&ItemCode=000004249&SearchNow=on&ProdName=Crown+Russe+Vodka+80+Proof&fromURL=%2Fwebapp%2Fwcs%2Fstores%2Fservlet%2FCatalogSearchResultView%3FstoreId%3D10051%26catalogId%3D10051%26langId%3D-1%26sType%3DSimpleSearch%26resultCatEntryType%3D2%26showResultsPage%3Dtrue%26pageSize%3D15%26sortBy%3D5%26searchSource%3DQ%26pageView%3D%26beginIndex%3D0%26searchTerm%3DCrown%26SearchKeyWord%3DCrown&countyName=Centre&f=&storeName=&pageNum=&perPage=&storeType=&city=&zip_code=&county=14&storeNO=" | grep "Units" | awk '{print $2}'> ./webPages/"$FILE_NAME"
+# Get the webpage via CURL
+WEBPAGE=$(curl -s "https://www.finewineandgoodspirits.com/webapp/wcs/stores/servlet/StoreStateSearch?storeId=10051&langId=-1&catalogId=10051&ItemCode=000004249&SearchNow=on&ProdName=Crown+Russe+Vodka+80+Proof&fromURL=%2Fwebapp%2Fwcs%2Fstores%2Fservlet%2FCatalogSearchResultView%3FstoreId%3D10051%26catalogId%3D10051%26langId%3D-1%26sType%3DSimpleSearch%26resultCatEntryType%3D2%26showResultsPage%3Dtrue%26pageSize%3D15%26sortBy%3D5%26searchSource%3DQ%26pageView%3D%26beginIndex%3D0%26searchTerm%3DCrown%26SearchKeyWord%3DCrown&countyName=Centre&f=&storeName=&pageNum=&perPage=&storeType=&city=&zip_code=&county=14&storeNO=")
+
+# Get the store street names as locations
+STORES=$(grep "</span><span class=\"normalText\">" <<< $WEBPAGE | cut -d ">" -f 3 | cut -d "&" -f 1) 
+#echo $STORES
+#grep "</span><span class=\"normalText\">" <<< $WEBPAGE | cut -d ">" -f 3 | cut -d "&" -f 1 > locations.txt
+
+# Get the inventory as units 
+UNITS=$(awk '/Units/{print $2}' <<< $WEBPAGE | sed 's/ /\n/g')
+#echo $UNITS
+#awk '/Units/{print $2}' <<< $WEBPAGE | sed 's/ /\n/g' > units.txt
+
+# Combine locations and units to CSV
+CSV_DATA=$(paste -d: <(echo "$STORES") <(echo "$UNITS") | paste -s -d, -)
+
+# Put locations into CSV in line with date/time
+echo "$FILE_DATE,$CSV_DATA" >> master.csv
+
